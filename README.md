49f05ae443d8a012c28b83b107af3b39b3e43291
Thomas Mitterfellner <thomas.mitterfellner@gmail.com>
2021-04-05 14:46:26 +0200

    Add Yamaha NP-S303 support/change to last.fm API v2.0
    - now using the last.fm.api (https://github.com/leemm/last.fm.api) which is a wrapper for Last.FM API v2 (Scrobbling 2.0, https://www.last.fm/api/scrobbling)
    - using "Now Playing" and only scrobbling after half of the track's duration or 4 min, whichever comes first
    - Added Yamaha NP-S303 (a nice network player I own)
    - code formatting (unfortunately; I don't really know javascript/node so I had to do that in order to wrap my head around what's going on, especially with this Promises concept. Maybe I'll come back one day when I have read up on jaavascript and proper formatting guidlines for js code, maybe I won't)
    - added an upstart script (to be used with a Synology NAS) for starting the scrobbler at boot

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # yamaha_musiccast_scrobbler
 
 ## Info
+This is (obviously) a fork of arekk/yamaha_musiccast_scrobbler
+The changes I made are:
+- now using the last.fm.api (https://github.com/leemm/last.fm.api) which is a wrapper for Last.FM API v2 (Scrobbling 2.0, https://www.last.fm/api/scrobbling)
+- using "Now Playing" and only scrobbling after half of the track's duration or 4 min, whichever comes first
+- Added Yamaha NP-S303 (a nice network player I own)
+- code formatting (unfortunately; I don't really know javascript/node so I had to do that in order to wrap my head around what's going on, especially with this Promises concept. Maybe I'll come back one day when I have read up on jaavascript and proper formatting guidlines for js code, maybe I won't)
+- added an upstart script (to be used with a Synology NAS) for starting the scrobbler at boot
+
+Below is the original README:
+
 Last.FM scrobbler for Yamaha MusicCast devices, ie. CRX-N470D
 
 Simple application to discover MusicCast devices in local network, check what is playing and scrobble it
--- last.js
+++ last.js
@@ -2,79 +2,79 @@ var ssdp = require('node-ssdp').Client;
 var _ = require('lodash');
 var http = require('http');
 var crypto = require('crypto');
-var lastFmApi = require('lastfmapi');
-fs = require('fs');
+
+const fs = require('fs');
+const API = require('last.fm.api');
+
+const updateInterval = 1000;
+const scrobbleAfter = 240000;
+const countBeforeScrobble = Math.trunc(scrobbleAfter/updateInterval);
 
 var ssdpClient;
 var devices = [];
 var currentSongs = [];
-var lastFmClient;
-var lastFmSession;
+var lastFMSession;
+var api;
 
 function initLastFmClient(onSuccess) {
-  fs.readFile('./secret.json', 'utf8', function (err, session) {
-    if (err) {
-      console.log('Config secret.json is missing, please create one');
-    } else {
-      var secrets = JSON.parse(session);
-      lastFmClient = new lastFmApi({'api_key': secrets.key, 'secret': secrets.secret});
-      onSuccess();
+  fs.readFile('./secret.json', 'utf8', 
+    function (err, session) {
+      if (err) {
+        console.log('Config secret.json is missing, please create one');
+      } else {
+        var secrets = JSON.parse(session);
+        api = new API({ 
+            apiKey: secrets.key, 
+            apiSecret: secrets.secret,
+            debug: true,
+            username: secrets.username,
+            password: secrets.password,
+          });
+        onSuccess();
+      }
     }
-  });
+  );
 }
 
 function authenticateLastFmUser(onSuccess) {
-  fs.readFile('./session.json', 'utf8', function (err, session) {
-    if (err) {
-      lastFmClient.auth.getToken(function (err, token) {
-        console.log('Go to url: http://www.last.fm/api/auth/?api_key=' + lastFmApiKey + '&token=' + token + ' give application access and then press any key to continue');
-        process.stdin.setRawMode();
-        process.stdin.resume();
-        process.stdin.on('data', function () {
-          lastFmClient.auth.getSession(token, function (err, newSession) {
-            if (err) {
-              console.log(err);
-            } else {
-              fs.writeFile('./session.json', JSON.stringify(newSession), function () {
-                lastFmSession = newSession;
-                console.log("Saved session");
-                console.log(lastFmSession);
-                onSuccess();
-              });
-            }
-          })
-        });
-      });
-    } else {
-      lastFmSession = JSON.parse(session);
-      console.log("Restored session");
-      console.log(lastFmSession);
-      onSuccess();
-    }
-  });
+  fs.readFile('./session.json', 'utf8', 
+    function (err, session) {
+      if (err) {
+        api.auth.getMobileSession({})
+          .then(json => json.session)
+	  .then(session => {
+              // Now that we have a session try to add some tags (a method that requires auth)
+              fs.writeFile('./session.json', JSON.stringify(session), function () {
+                  lastFMSession = session;
+                  console.log("Saved session");
+                  console.log(lastFMSession);
+                  onSuccess();
+                }
+              );
+	    })
+	  .catch(err => {
+	    console.error('ERRORED!', JSON.stringify(err));
+            process.exit();
+	  });
+      } else {
+        lastFMSession = JSON.parse(session);
+        console.log("Restored session");
+        console.log(lastFMSession);
+        onSuccess();
+      }
+    } // function
+  );
 }
 
-function sendScrobble(song) {
-  lastFmClient.setSessionCredentials(lastFmSession.name, lastFmSession.key);
-  lastFmClient.track.scrobble({
-    'artist': _.get(song, 'artist.name'),
-    'track': _.get(song, 'name'),
-    'timestamp': Math.floor((new Date()).getTime() / 1000)
-  }, function (err) {
-    var songInfo = [_.get(song, 'artist.name'), _.get(song, 'album.title', ''), _.get(song, 'name')];
-    if (err) {
-      console.log('Unable to scrobble', songInfo);
-    } else {
-      console.log('We have just scrobbled a song', songInfo);
-    }
-  });
+function sendScrobble(songInfoDict) {
+  console.log('Scrobbling ' + songInfoDict.artist + ' - ' + songInfoDict.track);
+  api.track.scrobble(songInfoDict);
 }
 
-function getProperSong(input, artist, album, track, onSuccess, onError) {
+function getProperSong(input, artist, album, track, funcSongFound, funcSongNotFound) {
   if (input == 'net_radio' && track.length > 0) {
-    var search = _.map(_.split(track, '-'), function (s) {
-      return _.trim(s);
-    });
+    var search = _.map(_.split(track, '-'), function (s) { return _.trim(s); });
+
     if (search.length == 3) {
       artist = search[0];
       track = search[2];
@@ -87,19 +87,15 @@ function getProperSong(input, artist, album, track, onSuccess, onError) {
     }
   }
   if (!artist || !track || artist.length == 0 || track.length == 0) {
-    onError('Missing artist or title - unable to identify a song');
+    funcSongNotFound('Missing artist or title - unable to identify a song');
   } else {
-    lastFmClient.track.getInfo({'artist': artist, 'track': track}, function (err, track) {
-      if (err) {
-        onError(err);
-      } else {
-        onSuccess(track);
-      }
-    });
+    api.track.getInfo({'artist': artist, 'track': track})
+      .then(json => { funcSongFound(json.track); })
+      .catch(err => { console.error(err); });
   }
 }
 
-function getSong(ip, input, onReady) {
+function getSong(ip, input, scrobbleSong) {
   var path;
   if (_.indexOf(['spotify', 'net_radio', 'server'], input) > -1) {
     path = '/YamahaExtendedControl/v1/netusb/getPlayInfo';
@@ -110,75 +106,123 @@ function getSong(ip, input, onReady) {
     http.get({
         host: ip,
         path: path
-      }, function (response) {
+      }, 
+      function (response) {
         var body = '';
-        response.on('data', function (d) {
-          body += d;
-        });
-        response.on('end', function () {
-          var parsed = JSON.parse(body);
-          if (_.get(parsed, 'playback') == 'play') {
-            if (input == 'cd') {
-              // yeah, cd-text has artist/track in invalid attributes
-              var artist = _.trim(_.get(parsed, 'album', '')), album = '', track = _.trim(_.get(parsed, 'artist', ''));
-            } else {
-              var artist = _.trim(_.get(parsed, 'artist', '')), album = _.trim(_.get(parsed, 'album', '')), track = _.trim(_.get(parsed, 'track', ''));
-            }
-
-            var current = _.get(currentSongs, ip);
-            var md5sum = crypto.createHash('md5').update(artist + album + track).digest('hex');
-
-            if (current == null || current.checksum != md5sum) {
-              var funcSongFound = function (song) {
-                console.log('Got ', [_.get(song, 'artist.name'), _.get(song, 'album.title', ''), _.get(song, 'name')], ' - waiting 10s before scrobble');
-                _.set(currentSongs, [ip], {checksum: md5sum, count: 0, scrobbled: false, song: song});
-              };
-              var funcSongNotFound = function (err) {
-                console.log('Not scrobbling:', err);
-                _.set(currentSongs, [ip], {checksum: md5sum, count: 0, scrobbled: true, song: null});
-              };
-              getProperSong(input, artist, album, track, funcSongFound, funcSongNotFound);
-            }
-            if (current != null && current.checksum == md5sum) {
-              _.set(currentSongs, [ip, 'count'], (current.count + 1));
-            }
-            if (current != null && current.checksum == md5sum && current.count > 10 && current.scrobbled == false && current.song != null) {
-              _.set(currentSongs, [ip, 'scrobbled'], true);
-              onReady(current.song);
-            }
+        response.on('data', 
+          function (d) {
+            body += d;
           }
-        });
-      }
-    );
+        );
+        response.on('end', 
+          function () {
+            var parsed = JSON.parse(body);
+            if (_.get(parsed, 'playback') == 'play') {
+              if (input == 'cd') {
+                // yeah, cd-text has artist/track in invalid attributes
+                var artist = _.trim(_.get(parsed, 'album', '')), album = '', track = _.trim(_.get(parsed, 'artist', ''));
+              } else {
+                var artist = _.trim(_.get(parsed, 'artist', '')), album = _.trim(_.get(parsed, 'album', '')), track = _.trim(_.get(parsed, 'track', ''));
+              }
+
+              var current = _.get(currentSongs, ip);
+              var md5sum = crypto.createHash('md5').update(artist + album + track).digest('hex');
+
+              if (current == null || current.checksum != md5sum) {
+                var funcSongFound = 
+                  // executed once when proper song information could be retrieved (by getProperSong)
+                  function (song) { 
+                    const halfSongDuration = song.duration/2.0;
+                    const timeBeforeScrobble = Math.min(halfSongDuration, scrobbleAfter);
+                    const countBeforeScrobble = Math.trunc(timeBeforeScrobble/updateInterval);
+
+                    console.log('Got ', [_.get(song, 'artist.name'), _.get(song, 'album.title', ''), _.get(song, 'name')], 
+                                         ' - waiting ' + timeBeforeScrobble/1000 + ' s before scrobble');
+
+                    const songInfoDict = {
+                      'artist': song.artist.name,
+                      'track': song.name,
+                      'sk': lastFMSession.key,
+                      'timestamp': Math.floor((new Date()).getTime() / 1000)
+                    };
+                    _.set(currentSongs, [ip], 
+                      { 
+                        checksum: md5sum, 
+                        count: 0, 
+                        scrobbled: false, 
+                        song: song,
+                        songInfoDict: songInfoDict,
+                        countBeforeScrobble: countBeforeScrobble
+                      }
+                    );
+
+                    // send info about current song to last fm (not yet the final scrobble!)
+                    api.track.updateNowPlaying(songInfoDict);
+                  }; // funcSongFound
+
+                var funcSongNotFound = 
+                  function (err) {
+                    console.log('Not scrobbling:', err);
+                    _.set(currentSongs, [ip], {checksum: md5sum, count: 0, scrobbled: true, song: null});
+                  }; // funcSongNotFound
+
+                getProperSong(input, artist, album, track, funcSongFound, funcSongNotFound);
+              }
+              // increase loop counter if song is still the same as before
+              if (current != null && current.checksum == md5sum) {
+                _.set(currentSongs, [ip, 'count'], (current.count + 1));
+              }
+              // check preconditions for the final scrobble
+              if (   current != null 
+                  && current.song != null
+                  && current.scrobbled == false
+                  && current.checksum == md5sum 
+                  && current.count > current.countBeforeScrobble
+                  )
+              {
+                _.set(currentSongs, [ip, 'scrobbled'], true);
+                scrobbleSong(current.songInfoDict);
+              }
+            }
+          } // function (response.on)
+        ); // response.on
+      } // function (http.get)
+    ); // http.get
   }
 }
 
 function getInput(ip, onPowerOn) {
   http.get({
-    host: ip,
-    path: '/YamahaExtendedControl/v1/main/getStatus'
-  }, function (response) {
-    var body = '';
-    response.on('data', function (d) {
-      body += d;
-    });
-    response.on('end', function () {
-      var parsed = JSON.parse(body);
-      if (_.get(parsed, 'power') == 'on') {
-        onPowerOn(ip, _.get(parsed, 'input'));
-      }
-    });
-  });
+      host: ip,
+      path: '/YamahaExtendedControl/v1/main/getStatus'
+    }, 
+    function (response) {
+      var body = '';
+      response.on('data', function(d) { body += d; });
+      response.on('end', function () {
+        var parsed = JSON.parse(body);
+        if (_.get(parsed, 'power') == 'on') {
+          onPowerOn(ip, _.get(parsed, 'input'));
+        }
+      });
+    }
+  ); // http.get
 }
 
 function playInfoLoop() {
-  _.each(devices, function (ip) {
-    getInput(ip, function (ip, input) {
-      getSong(ip, input, function (song) {
-        sendScrobble(song);
-      })
-    })
-  });
+  _.each(devices, 
+    function (ip) {
+      getInput(ip, 
+        function (ip, input) {
+          getSong(ip, input, 
+            function (song) {
+              sendScrobble(song);
+            }
+          )
+        }
+      )
+    }
+  );
 }
 
 function initSsdpClient(onReady) {
@@ -189,7 +233,12 @@ function initSsdpClient(onReady) {
 
 function processSsdpResponse(headers, code, rinfo) {
   var model = _.get(headers, 'X-MODELNAME');
-  if (model && (model.indexOf('CRX-') !== -1 || model.indexOf('WX-') !== -1)) {
+  if (   model 
+      && (   model.indexOf('CRX-') !== -1 
+          || model.indexOf('WX-') !== -1 
+          || model.indexOf('NP-S303') !== -1)
+     ) 
+  {
     if (_.indexOf(devices, rinfo['address']) == -1) {
       devices.push(rinfo['address']);
     }
@@ -204,21 +253,31 @@ function ssdpSearch() {
   }, 2000);
 }
 
+process.title = 'ymc_scrobbler'
+console.log(process.title);
+
 // loop SSDP device discovery every 1 minute
 initSsdpClient(function () {
   ssdpSearch();
   setInterval(ssdpSearch, (1000 * 60));
 });
 
-//  expire device list every 15 minutes
-setInterval(function () {
-  console.log('Resetting devices list');
-  devices = [];
-}, (1000 * 60 * 15));
+// expire device list every 15 minutes
+setInterval(
+  function () {
+    console.log('Resetting devices list');
+    devices = [];
+  }, 
+  (1000 * 60 * 15)
+);
 
 // login user info a Last.FM and loop current song check/scrobble every 1s
-initLastFmClient(function () {
-  authenticateLastFmUser(function () {
-    setInterval(playInfoLoop, 1000);
-  })
-});
+initLastFmClient(
+  function () {
+    authenticateLastFmUser(
+        function () {
+           setInterval(playInfoLoop, updateInterval);
+        }
+    )
+  }
+);
